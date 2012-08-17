89f096115da6f337995719ffdafc4b40cc8904cd
Zachery Hostens <zacheryph@gmail.com>
2012-08-17 11:32:45 -0500

    replace composed_of with actual accessors

--- lib/money/rails.rb
+++ lib/money/rails.rb
@@ -9,32 +9,22 @@ module ActiveRecord #:nodoc:
 
       module ClassMethods
         def money(name, options = {})
-          allow_nil = options.has_key?(:allow_nil) ? options.delete(:allow_nil) : true
-          rounding  = options.has_key?(:round)     ? options.delete(:round)     : nil
-          options = {:precision => 2, :cents => "#{name}_in_cents".to_sym }.merge(options)
-          mapping = [[options[:cents], 'cents']]
-          mapping << [options[:currency].to_s, 'currency'] if options[:currency]
-          composed_of name, :class_name => 'Money', :mapping => mapping, :allow_nil => allow_nil,
-            :converter => lambda{ |m|
-              if !allow_nil && m.nil?
-                currency = options[:currency] || ::Money.default_currency
-                m = ::Money.new(0, currency, options[:precision])
-              end
-              ret = m.to_money(options[:precision])
-              rounding ? ret.to_d.round(rounding).to_money(options[:precision]) : ret
-            },
-            :constructor => lambda{ |*args|
-              cents, currency = args
-              cents ||= 0
-              currency ||= ::Money.default_currency
-              ::Money.new(cents, currency, options[:precision])
-            }
+          allow_nil  = options.has_key?(:allow_nil) ? options.delete(:allow_nil) : true
+          field_name = options[:cents] || "#{name}_in_cents"
+          precision  = options[:precision] || 2
+          currency   = (options[:currency] || ::Money.default_currency).to_s
+          rounding   = options[:round]
 
-          define_method "#{name}_with_cleanup=" do |amount|
-            amount = nil if amount.blank?
-            send "#{name}_without_cleanup=", amount
+          define_method "#{name}" do
+            val = self.read_attribute(field_name)
+            val ? ::Money.new(self.read_attribute(field_name), currency, precision) : val
           end
-          alias_method_chain "#{name}=", :cleanup
+
+          define_method "#{name}=" do |val|
+            val = 0 if !allow_nil && val.blank?
+            val = BigDecimal.new(val.to_s).to_money(precision).round(rounding || precision)
+            self.send(:write_attribute, field_name, val.cents)
+          end
         end
       end
     end
